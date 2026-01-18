531ef6251e798394da281d043cb26c4c38aa5612
Sudarshan <128029723+realsudarshan@users.noreply.github.com>
2026-01-18 14:02:30 +0545

    fix(icons):  Icons added in current streak and best streak

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -1,7 +1,7 @@
 // Fallback for using MaterialIcons on Android and web.
 
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { SymbolWeight, SymbolViewProps } from 'expo-symbols';
+import { SymbolViewProps, SymbolWeight } from 'expo-symbols';
 import { ComponentProps } from 'react';
 import { OpaqueColorValue, type StyleProp, type TextStyle } from 'react-native';
 
@@ -18,6 +18,12 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'flame.fill': 'local-fire-department',
+  'zzz': 'bedtime',
+  'trophy.fill': 'emoji-events',
+  'target': 'track-changes',
+  'plus.circle.fill': 'add-circle',
+  'book.fill': 'book',
 } as IconMapping;
 
 /**
